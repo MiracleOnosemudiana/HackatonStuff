ed9ebdcaff59ad78f67dbfaec4691f880f98fd53
jamiebones <jamiebones2000@yahoo.co.uk>
2023-08-01 14:49:31 +0100

    fix small changes on the main branch

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiOutLineMenu, AiOutLineClose } from 'react-icons'
+// import { AiOutLineMenu, AiOutLineClose } from 'react-icons'
 
 
 const Navbar = () => {
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Hero from "@/components/Hero";
+import Hero from "../components/Hero";
 import Slider from "@/components/Slider";
 import { SliderData } from "../components/SliderData";
 
